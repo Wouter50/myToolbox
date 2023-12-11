767dd79616f55c3da793fec9af3f40d1e3310208
Wouter50 <wouterdeboer5@gmail.com>
2023-12-11 21:41:57 +0100

    fixed heads or tails outcome, changed theme

--- App/app/src/main/java/com/example/mytoolbox/ui/HeadsOrTailsActivity.kt
+++ App/app/src/main/java/com/example/mytoolbox/ui/HeadsOrTailsActivity.kt
@@ -1,10 +1,13 @@
 package com.example.mytoolbox.ui
 
 import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextSwitcher
 import android.widget.TextView
+import android.widget.Toast
 import android.widget.ViewSwitcher
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mytoolbox.MainActivity
@@ -12,18 +15,20 @@ import com.example.mytoolbox.R
 
 
 class HeadsOrTailsActivity : AppCompatActivity() {
+    lateinit var coinImageView: ImageView
+    lateinit var headsOrTailsButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_heads_or_tails)
 
 
         val backButton = findViewById<Button>(R.id.hodbackButton)
-        val headsOrTailsButton = findViewById<Button>(R.id.TossCoin)
-        var textViewResult = findViewById<TextSwitcher>(R.id.headsOrTailsResult)
-        textViewResult.setFactory { TextView(this) }
+        headsOrTailsButton = findViewById<Button>(R.id.TossCoin)
 
-        textViewResult.setInAnimation(this, android.R.anim.slide_in_left)
-        textViewResult.setOutAnimation(this, android.R.anim.slide_out_right)
+
+
+
+        coinImageView = findViewById<ImageView>(R.id.coinImageView)
 
 
         backButton.setOnClickListener{
@@ -32,14 +37,27 @@ class HeadsOrTailsActivity : AppCompatActivity() {
         }
 
         headsOrTailsButton.setOnClickListener{
-            val headsOrTailsResultString = when ((1..2).random()) {
-                1 -> "Heads"
-                2 -> "Tails"
-                else -> {
-                    "Error, Please retry"
-                }
+            val randomNumber = (1..2).random()
+
+            if (randomNumber == 1){
+                flipTheCoin(R.drawable.ic_coin_heads, "Heads")
+            } else {
+                flipTheCoin(R.drawable.ic_coin_tails, "Tails")
             }
-            textViewResult.setText(headsOrTailsResultString)
+
+        }
+
+    }
+    private fun flipTheCoin(imageId: Int, coinSide:String){
+        coinImageView.animate().apply {
+            coinImageView.setImageResource(R.drawable.ic_coin_empty)
+            duration = 1000
+            rotationYBy(1800f)
+            headsOrTailsButton.isClickable = false
+        }.withEndAction {
+            coinImageView.setImageResource(imageId)
+            Toast.makeText(this, coinSide, Toast.LENGTH_SHORT).show()
+            headsOrTailsButton.isClickable = true
         }
     }
 }
